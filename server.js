8c5e865998da3982a7bbf822223191695e018d2f
immy458 <59685135+immy458@users.noreply.github.com>
2020-11-24 02:10:13 +0530

    .

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
-
-const PORT = 4000;
+const pool = require("../handajitech/connection");
 const app = express();
 app.use(cors());
 app.use(express.json()); // to recognize the incoming Request Object as a JSON Object.
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1522",
-  database: "handajitech",
-});
-connection.connect(function (err) {
-  err ? console.log(err) : console.log(connection);
-});
-
 app.get("/", function (req, res) {
-  connection.query("SELECT * FROM startup", function (err, data) {
+  pool.query("SELECT * FROM startup", function (err, data) {
     err ? res.send(err) : res.json({ startuplist: data });
   });
 });
@@ -27,11 +16,11 @@ app.post("/api/savedata", function (req, res) {
   const country = req.body.country;
   console.log(name);
   const insert_query = `INSERT INTO startup (name,country) VALUES('${name}','${country}')`;
-  connection.query(insert_query, function (err, data) {
+  pool.query(insert_query, function (err, data) {
     err ? res.send(err) : res.send("Successfully added!!!");
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`App running on port: ${PORT}`);
+app.listen(process.env.APP_PORT, () => {
+  console.log("App running on port!!:", process.env.APP_PORT);
 });
